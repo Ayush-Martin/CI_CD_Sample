a630cb33fa56fbe5a950568472ddb9ace6338036
Ayush Martin Sabu <ayushmartin06@gmail.com>
2025-08-02 12:35:20 +0530

    updated new

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ redisClient.connect();
 // Routes
 
 app.get("/", (req, res) => {
-  res.send("Hello World dfd dfdf");
+  res.send("Hello World dfd dfdfd");
 });
 
 app.post("/add-name", async (req, res) => {
