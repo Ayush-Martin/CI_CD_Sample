e7a2e4279b1840c205b97f2b351117619b2d5b03
Ayush Martin Sabu <ayushmartin06@gmail.com>
2025-08-02 12:32:28 +0530

    updated new

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
-set -e
-
-echo "$ENV_FILE" > .env
-
-ssh -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key $EC2_USER@$EC2_HOST << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no -i ~/.ssh/deploy_key "$EC2_USER@$EC2_HOST" << 'ENDSSH'
   mkdir -p ~/ci-cd-app
   cd ~/ci-cd-app
 
-  echo "$ENV_FILE" > .env
+  cat <<EOF > .env
+APP_NAME=$APP_NAME
+PORT=$PORT
+REDIS_URL=$REDIS_URL
+EOF
 
   docker pull ayushmartin/ci_cd_sample:latest
   docker stop ci_cd_sample || true && docker rm ci_cd_sample || true
